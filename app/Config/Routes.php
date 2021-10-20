70b92bb1d18ba0855c86f228b23d912a1e2fcf9a
unknown <afif.azizy@gmail.com>
2021-10-20 23:03:38 +0700

    Add pencarian db buku di landing page

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -32,6 +32,7 @@ $routes->setAutoRoute(true);
 // We get a performance increase by specifying the default
 // route since we don't have to scan directories.
 $routes->get('/', 'Home::index');
+$routes->get('/database', 'Home::databaseBuku');
 $routes->get('/admin', 'Admin::index');
 $routes->group('admin', function ($routes) {
     $routes->add('login', 'Admin::login');
