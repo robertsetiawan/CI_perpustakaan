62aa34b6aaacf259d83e2960f0922fde51004482
Jeremy Edbert Widjaja <widjajajeremy61@gmail.com>
2021-10-20 18:24:37 +0700

    menambah halaman available book

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -6,13 +6,14 @@ use App\Models\BookModel;
 use App\Models\PeminjamanModel;
 use App\Models\CategoryModel;
 use App\Models\MemberModel;
+use App\Models\TransaksiModel;
 
 class Dashboard extends BaseController
 {
     public function index()
     {
         $session = session();
-        $data['title'] = 'PERPUS|Dashboard';
+        $data['title'] = 'PERPUS | Dashboard';
         $data['username'] = $session->get('username');
 
         return view('dashboard_admin', $data);
@@ -25,7 +26,8 @@ class Dashboard extends BaseController
         return view('dashboard_list_book', $data);
     }
 
-    public function getAvailBook(){
+    public function getAvailBook()
+    {
         $books = new BookModel();
         // $query = $books->query('SELECT * FROM buku WHERE stok_tersedia = 0');
         $data['books'] = $books->where('stok_tersedia !=', 0)->findAll();
@@ -33,11 +35,20 @@ class Dashboard extends BaseController
         // $data['books'] = $books;
     }
 
-    public function getBorrowedBook(){
+    public function getBorrowedBook()
+    {
         $peminjaman = new PeminjamanModel();
 
         $data['peminjaman'] = $peminjaman->getAllTransaction();
         return view('dashboard_borrowed_book', $data);
+    }
+
+    public function getReturnedBook()
+    {
+        $pengembalian = new TransaksiModel();
+
+        $data['pengembalian'] = $pengembalian->getAllTransaction();
+        return view('dashboard_returned_book', $data);
     }
 
     public function addBook()
@@ -70,11 +81,27 @@ class Dashboard extends BaseController
             }
         }
     }
-    
+
     public function getAllMembersFromDatabase()
     {
         $members = new MemberModel();
         $data['members'] = $members->findAll();
         return view('dashboard_db-list_anggota', $data);
+    }
+
+    public function updatePengembalian($idtransaksi, $idbuku, $tgl_pinjam)
+    {
+        $trans = new TransaksiModel();
+        
+        // echo (strtotime(date('Y-m-d'))-strtotime($tgl_pinjam))/86400;
+
+        // $date = date('Y-m-d', strtotime($tgl_pinjam));
+        if((strtotime(date('Y-m-d'))-strtotime($tgl_pinjam))/86400 < 14){
+            $trans->updateTransaction($idtransaksi, $idbuku);
+        } else{
+            $trans->updateTransactionFine($idtransaksi, $idbuku);
+        }
+
+        return redirect()->to(base_url('dashboard/borrowed_book'));
     }
 }
