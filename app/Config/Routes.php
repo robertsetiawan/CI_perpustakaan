3d3d4fbd9396f7e51ddd0632f308f7e5c3aa2309
Robert Setiawan <72391955+robertsetiawan@users.noreply.github.com>
2021-10-21 20:38:29 +0700

    production deploy

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -17,7 +17,7 @@ if (file_exists(SYSTEMPATH . 'Config/Routes.php')) {
  * --------------------------------------------------------------------
  */
 $routes->setDefaultNamespace('App\Controllers');
-$routes->setDefaultController('Admin');
+$routes->setDefaultController('Home');
 $routes->setDefaultMethod('index');
 $routes->setTranslateURIDashes(false);
 $routes->set404Override();
