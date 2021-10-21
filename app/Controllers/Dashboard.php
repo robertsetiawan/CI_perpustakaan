f63baefc98c97e5c198fbf73c781773eb2021bec
unknown <afif.azizy@gmail.com>
2021-10-21 14:32:15 +0700

    Menambahkan peminjaman buku

--- app/Controllers/Dashboard.php
+++ app/Controllers/Dashboard.php
@@ -67,4 +67,34 @@ class Dashboard extends BaseController
 
         return redirect()->to(base_url('dashboard/borrowed_book'));
     }
+
+    public function addBorrowedBook(){
+        $books = new BookModel();
+        $data['availBooks'] = $books->where('stok_tersedia !=', 0)->findAll();
+        return view('dashboard_addBorrowedBook.php', $data);
+    }
+
+    public function processBorrowedBook(){
+        $books = $this->request->getPost('books');
+        $member = new MemberModel();
+        $transaksi = new TransaksiModel();
+
+        $dataMember = $member->where('nim', $this->request->getVar('nim'))->first();
+        if (empty($dataMember)){
+            session()->setFlashdata('error', "NIM tidak tersedia");
+            return redirect()->back()->withInput();
+        }
+        if (empty($books)){
+            session()->setFlashdata('error', "Pilih buku yang dipinjam");
+            return redirect()->back()->withInput();
+        }
+        if (!count($books)<3){
+            session()->setFlashdata('error', "Buku yang dipinjam maksimal 2 buku");
+            return redirect()->back()->withInput();
+        }
+        if (!empty($this->request->getVar('nim')) and !empty($books) and count($books)<3){
+            $transaksi->insertBorrowedBook((int)round(gettimeofday(true)), $this->request->getVar('nim'), session()->get('id'), $books);
+        }
+        return redirect()->to(base_url('dashboard/borrowed_book'));
+    }
 }
